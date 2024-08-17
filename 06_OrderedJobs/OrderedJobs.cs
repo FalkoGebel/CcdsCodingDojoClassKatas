f636cd14cf05c748c229a75c62e5146c0e5f708f
Falko Gebel <falko-gebel@live.de>
2024-08-17 10:01:16 +0200

    - worked on and finished Ordered Jobs

---
+++ 06_OrderedJobs/OrderedJobs.cs
@@ -0,0 +1,97 @@
+﻿namespace _06_OrderedJobs
+{
+    public class OrderedJobs : IOrderedJobs
+    {
+        Dictionary<char, List<char>> _jobs;
+
+        public OrderedJobs()
+        {
+            _jobs = [];
+        }
+
+        public void Register(char dependentJob, char independentJob)
+        {
+            if (dependentJob == independentJob)
+                throw new ArgumentException("Job can not be dependent from itself");
+
+            CheckJob(dependentJob);
+            CheckJob(independentJob);
+
+            if (!_jobs.ContainsKey(dependentJob))
+                _jobs[dependentJob] = [independentJob];
+            else if (!_jobs[dependentJob].Contains(independentJob))
+                _jobs[dependentJob].Add(independentJob);
+
+            Register(independentJob);
+        }
+
+        public void Register(char job)
+        {
+            CheckJob(job);
+
+            if (!_jobs.ContainsKey(job))
+                _jobs[job] = [];
+        }
+
+        public void Register(string registrations)
+        {
+            string[] registrationArray = registrations.Split('\n');
+
+            if (registrationArray.Length == 0)
+                throw new ArgumentException("Invalid registration string");
+
+            foreach (string registration in registrationArray)
+            {
+                string[] parts = registration.Split("=>", System.StringSplitOptions.RemoveEmptyEntries)
+                                             .Select(r => r.Trim())
+                                             .ToArray();
+
+                if (parts.Length < 1 || parts.Length > 2 || parts.Any(p => p.Length != 1))
+                    throw new ArgumentException("Invalid registration string");
+
+                if (parts.Length == 1)
+                    Register(parts[0][0]);
+                else
+                    Register(parts[0][0], parts[1][0]);
+            }
+        }
+
+        public string Sort()
+        {
+            string output = string.Concat(_jobs.Where(j => j.Value.Count == 0).ToDictionary().Keys);
+            List<char> dependentJobs = _jobs.Where(j => j.Value.Count > 0).ToDictionary().Keys.ToList();
+
+            while (dependentJobs.Count > 0)
+            {
+                List<int> validIndexes = [];
+
+                for (int i = 0; i < dependentJobs.Count; i++)
+                {
+                    char currentJob = dependentJobs[i];
+
+                    if (_jobs[currentJob].All(j => output.Contains(j)))
+                    {
+                        output += currentJob;
+                        validIndexes.Add(i);
+                    }
+                }
+
+                if (validIndexes.Count == 0)
+                    throw new InvalidOperationException("Operation not possible - there are circular dependencies in the data");
+
+                validIndexes.Reverse();
+                foreach (int i in validIndexes)
+                    dependentJobs.RemoveAt(i);
+            }
+
+            return output;
+        }
+
+        private void CheckJob(char job)
+        {
+            if (!char.IsLetter(job) || !char.IsLower(job))
+                throw new ArgumentException("Invalid job - only lower letters allowed");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ In this repository I am presenting my solutions for the class katas of the [Clea
 + 02: [Bowling](https://ccd-school.de/en/coding-dojo/classes-katas/bowling/ "Bowling") -> [Done](/02_Bowling/ "Project Folder")
 + 03: [Box Plot](https://ccd-school.de/en/coding-dojo/classes-katas/box-plot/ "Box Plot") -> [Done](/03_BoxPlot/ "Project Folder")
 + 04: [Hangman](https://ccd-school.de/coding-dojo/class-katas/galgenmaennchen/ "Hangman") -> [Done](/04_Hangman/ "Project Folder")
-+ 05: [Linked List](https://ccd-school.de/en/coding-dojo/classes-katas/linked-list/ "Linked List") -> [Done](/05LinkedList/ "Project Folder")
++ 05: [Linked List](https://ccd-school.de/en/coding-dojo/classes-katas/linked-list/ "Linked List") -> [Done](/05__LinkedList/ "Project Folder")
++ 06: [Ordered Jobs](https://ccd-school.de/en/coding-dojo/classes-katas/ordered-jobs/ "Ordered Jobs") -> [Done](/06_OrderedJobs/ "Project Folder")
