e975fa7b4da97c9e4268ecfa1c472bcd657eb6f6
Falko Gebel <falko-gebel@live.de>
2024-08-18 22:39:23 +0200

    - worked on and finished Circular Buffer

---
+++ 08_CircularBuffer/CircularBuffer.cs
@@ -0,0 +1,48 @@
+﻿namespace _08_CircularBuffer
+{
+    public class CircularBuffer<T>
+    {
+        private readonly int _size;
+        private Queue<T> _elements;
+
+        public bool SilentReplacement { get; set; } = true;
+
+        public CircularBuffer(int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException("Invalid size - has to be positive integer");
+
+            _size = size;
+            _elements = new Queue<T>();
+        }
+
+        public void Add(T element)
+        {
+            if (Count() == Size())
+            {
+                if (!SilentReplacement)
+                    throw new InvalidOperationException("Circular Buffer is full and replacement not allowed");
+
+                _elements.Dequeue();
+            }
+
+            _elements.Enqueue(element);
+        }
+
+        public int Count() => _elements.Count;
+
+        public int Size() => _size;
+
+        public T Take()
+        {
+            try
+            {
+                return _elements.Dequeue();
+            }
+            catch (InvalidOperationException e)
+            {
+                throw new InvalidOperationException("Circular Buffer is empty");
+            }
+        }
+    }
+}
